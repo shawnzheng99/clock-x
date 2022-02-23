230ee624aa1b67ea992045e7bef8ee50115f2253
Shawn Zheng <shawnz@checkpay.ca>
2022-02-23 13:46:20 -0800

    added moment driver

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { NumberLine } from './components';
 import CSS from 'csstype';
+import moment from 'moment';
+
 
 function App() {
+    const [ currNum, setCurrNum ] = useState<number>(0)
+
+    const [hour, setHour] = useState<string>('0')
+    const [min, setMin] = useState<string>('0')
+    const [sec, setSec] = useState<string>('0')
+
+    let intv: any ;
+    useEffect(() => {
+        
+        intv = setInterval(() => {
+            setHour(moment('DD MM YYYY hh:mm:ss').hour() + '')
+            setMin(moment().minute() + '')
+            setSec(moment().second() + '')
+            if(currNum === 9) {
+                setCurrNum(0)
+            }else {
+                setCurrNum(currNum + 1)
+            }
+        }, 1000)
+        return () => clearInterval(intv)
+    })
+
     return (
         <div className="App">
             <header className="App-header">
@@ -13,14 +37,14 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <div style={digiContainerStyle}>
-                    <NumberLine maxNumber={3} />
-                    <NumberLine maxNumber={10} />
+                    <NumberLine maxNumber={3}  selectedNumber={hour.split('')[0]} />
+                    <NumberLine maxNumber={10}  selectedNumber={hour.split('')[1]} />
 
-                    <NumberLine maxNumber={6} />
-                    <NumberLine maxNumber={10} />
+                    <NumberLine maxNumber={6}  selectedNumber={min.split('')[0]} />
+                    <NumberLine maxNumber={10} selectedNumber={min.split('')[1]} />
 
-                    <NumberLine maxNumber={6} />
-                    <NumberLine maxNumber={10} />
+                    <NumberLine maxNumber={6} selectedNumber={sec.split('')[0]} />
+                    <NumberLine maxNumber={10} selectedNumber={sec.split('')[1]} />
                 </div>
             </header>
         </div>
--- src/components/NumberLine.tsx
+++ src/components/NumberLine.tsx
@@ -2,17 +2,18 @@ import CSS from 'csstype';
 
 interface NumberLineProps {
     maxNumber: number,
-
+    selectedNumber?: number | string,
 }
 
-export const NumberLine = ({maxNumber}: NumberLineProps) => {
+export const NumberLine = ({ maxNumber, selectedNumber }: NumberLineProps) => {
     //@ts-ignore
-    const numbersLine: number [] = [...Array(maxNumber).keys()]
+    const numbersLine: number [] = [...Array(maxNumber).keys()];
+
     return (
         <div style={containerStyle}>
             {
                 numbersLine.map(n => (
-                    <span style={numberBoxStyle} key={'_num' + n}>{n}</span>
+                    <span style={n == selectedNumber? selectedNumberBox: numberBoxStyle} key={'_num' + n}>{n}</span>
                 ))
             }
         </div>
@@ -24,13 +25,20 @@ const containerStyle: CSS.Properties = {
     flexDirection: 'column',
     backgroundColor: '#5c5d5e',
     borderRadius: '5px',
-    
+    boxShadow: '.125em .25em .5em .125em #fff'
 }
 
 const numberBoxStyle: CSS.Properties = {
     borderRadius: '10px',
-    padding: '20px',
-    color: '#d9dcde', // selected color #fff
+    padding: '16px',
+    color: '#adadad',
     textShadow: '2px 1px #000',
+    fontSize: '0.5rem',
+};
+
+const selectedNumberBox: CSS.Properties = {
+    ...numberBoxStyle,
     fontSize: '1rem',
+    padding: '8px',
+    color: '#fff',
 };
